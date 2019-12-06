8e5539cdd221945c05e63a42aad8e6d2efba8645
ngorden <ngorden1997@gmail.com>
2019-12-06 12:08:20 -0600

    Add helmet

--- src/Server.ts
+++ src/Server.ts
@@ -6,10 +6,12 @@ import apiRouter from './routes/api'
 import cookieParser from 'cookie-parser'
 import createError from 'http-errors'
 import express from 'express'
+import helmet from 'helmet'
 import morgan from 'morgan'
 
 const app = express()
 
+app.use(helmet())
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(json({ limit: '1mb' }))
