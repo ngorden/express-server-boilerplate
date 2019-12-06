080dd159db0e053103f9525d84bb319f6e1902bb
Nicholas Gorden <ngorden1997@gmail.com>
2019-12-06 12:03:18 -0600

    Check for Front-End
    Safely copy the front-end by checking if it exists

--- build/build.js
+++ build/build.js
@@ -1,12 +1,21 @@
+import { copySync, existsSync, removeSync } from 'fs-extra'
 import { exec } from 'child_process'
-import { copySync, removeSync } from 'fs-extra'
+import { join } from 'path'
 
 try {
     // Remove existing dist files
     removeSync('./dist')
 
+    const publicDir = join(__dirname, '..', 'src', 'public')
+    const viewsDir = join(__dirname, '..', 'src', 'views')
+
     // Copy static files
-    copySync('./src/public', './dist/public')
+    if (existsSync(publicDir)) {
+        copySync(publicDir, publicDir.replace('src', 'dist'))
+    }
+    if (existsSync(viewsDir)) {
+        copySync(viewsDir, viewsDir.replace('src', 'dist'))
+    }
 
     exec('tsc --build tsconfig.json')
 } catch (err) {
